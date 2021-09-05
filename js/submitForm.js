29605b9ff5ea5ac19d0ec1b6fc68ff62228a48ae
Chris Hudson <ripya420@gmail.com>
2021-09-05 14:19:14 +0200

    Add Google Analytics event for form submissions

--- js/submitForm.js
+++ js/submitForm.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
 
     const form = document.getElementById("my-form");
     const button = document.getElementById("my-form-button");
@@ -16,18 +16,18 @@ window.addEventListener("DOMContentLoaded", function() {
         status.innerHTML = "Oops! There was a problem.";
     }
 
-    form.addEventListener("submit", function(ev) {
+    form.addEventListener("submit", function (ev) {
         ev.preventDefault();
         const data = new FormData(form);
-        ajax(form.method, form.action, data, success, error);
+        ajax(form.method, form.action, data, success, error, { formTitle: form.dataset.formTitle, eventCategory: form.dataset.eventCategory });
     });
 });
 
-function ajax(method, url, data, success, error) {
+function ajax(method, url, data, success, error, analyticParams) {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState !== XMLHttpRequest.DONE) return;
         if (xhr.status === 200) {
             success();
@@ -35,5 +35,6 @@ function ajax(method, url, data, success, error) {
             error(xhr.status, xhr.response, xhr.responseType);
         }
     };
+    gtag("event", `send_${analyticParams.formTitle}_form`, { 'event_category': analyticParams.eventCategory, 'event_label': "landing_area", 'non_interaction': false });
     xhr.send(data);
 }
