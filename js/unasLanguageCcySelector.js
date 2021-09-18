7d2b6ac877d166cba4e5dbda3e6ba589fb366261
Chris Hudson <ripya420@gmail.com>
2021-09-18 10:35:21 +0200

    Reformat

--- js/unasLanguageCcySelector.js
+++ js/unasLanguageCcySelector.js
@@ -15,20 +15,20 @@ function myAjaxRequest(method, url, data, success, error, analyticParams) {
 
 if (!document.referrer.includes("shop.nicosupply.eu")) {
     const availableLanguagesWithCurrencies = new Map([
-        ["en", {link: "https://shop.nicosupply.eu/", ccy: -1}],
-        ["pl", {link: "https://shop.nicosupply.eu/pl/", ccy: 0}],
-        ["cz", {link: "https://shop.nicosupply.eu/cz/", ccy: 1}],
-        ["ro", {link: "https://shop.nicosupply.eu/", ccy: 2}],
-        ["hu", {link: "https://shop.nicosupply.eu/sk/", ccy: 3}],
-        ["hr", {link: "https://shop.nicosupply.eu/", ccy: 4}],
-        ["sk", {link: "https://shop.nicosupply.eu/sk/", ccy: -1}],
-        ["de", {link: "https://shop.nicosupply.eu/de/", ccy: -1}]
+        ["en", { link: "https://shop.nicosupply.eu/", ccy: -1 }],
+        ["pl", { link: "https://shop.nicosupply.eu/pl/", ccy: 0 }],
+        ["cz", { link: "https://shop.nicosupply.eu/cz/", ccy: 1 }],
+        ["ro", { link: "https://shop.nicosupply.eu/", ccy: 2 }],
+        ["hu", { link: "https://shop.nicosupply.eu/sk/", ccy: 3 }],
+        ["hr", { link: "https://shop.nicosupply.eu/", ccy: 4 }],
+        ["sk", { link: "https://shop.nicosupply.eu/sk/", ccy: -1 }],
+        ["de", { link: "https://shop.nicosupply.eu/de/", ccy: -1 }]
     ]);
     const userLanguages = navigator.languages;
     const availableLanguages = [...availableLanguagesWithCurrencies.keys()];
     const selectedLanguageWithCountryCode = userLanguages.find(userLanguage => availableLanguages.some(language => userLanguage.toLowerCase().includes(language.toLowerCase()))) || "en";
     const selectedLanguage = availableLanguages.find(language => selectedLanguageWithCountryCode.includes(language));
-    
+
     if (selectedLanguage !== "en") {
         const selectedValues = availableLanguagesWithCurrencies.get(selectedLanguage);
         const formData = new FormData();
